6cf4519641c9cd1434b2a30e2891be9c8b51ff2c
James Tu <jamestvvo@gmail.com>
2024-02-11 02:42:08 -0500

    Implement saving pdf functionality

--- src/components/customize/customize-item.tsx
+++ src/components/customize/customize-item.tsx
@@ -1,7 +1,25 @@
 // import { useState } from "react";
-import '../../styles/customize.css'
+import '../../styles/customize.css';
+import jsPDF from 'jspdf';
 
 const CustomizeBar = () => {
+  const generatePDF = () => {
+
+    const resumeContainer = document.querySelector(".resume-container")?.cloneNode(true) as HTMLElement;
+
+    resumeContainer.style.width = "620px";
+    // resumeContainer.style.height = "842px";
+    resumeContainer.style.marginLeft = "auto";
+    resumeContainer.style.marginRight = "auto";
+
+    if(resumeContainer) {
+      const resume = new jsPDF('portrait', 'pt', 'a4');
+      resume.html(resumeContainer as HTMLElement)
+      ?.then(() => resume.save("resume.pdf"));
+    }
+  }
+
+
   return (
     <div className="customize-bar">
       <div className="customize-bar-content-container">
@@ -12,7 +30,7 @@ const CustomizeBar = () => {
         <div className="icon" id="customize-icon"></div>
         <p>Customize</p>
       </div>
-      <div className="customize-bar-save-resume-contaner">
+      <div className="customize-bar-save-resume-contaner" onClick={generatePDF}>
         <div className="icon" id="save-icon"></div>
         <p>Save Resume</p>
       </div>
